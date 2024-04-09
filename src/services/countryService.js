e7e3cb5c218474a847bc56f9d72e9ac10c2cca02
Anna <cameaann@gmail.com>
2024-04-09 12:48:49 +0300

    changed link to api

--- src/services/countryService.js
+++ src/services/countryService.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 const url = "https://studies.cs.helsinki.fi/restcountries/api/all";
-// const url = "https://restcountries.com/v3.1/all";
 
 const getCountries = () => {
   const promise = axios.get(url);
